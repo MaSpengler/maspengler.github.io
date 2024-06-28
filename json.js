da467c88d73617137879381f4b0b2b3a94008177
MaSpengler <163019171+MaSpengler@users.noreply.github.com>
2024-06-28 14:21:12 +0200

    Update json.js

--- json.js
+++ json.js
@@ -1,9 +1,8 @@
 $().ready(function(){
     $.getJSON( "/data.json", function(data) {
     console.log(data);
-    //$("#text").html(data["text"]);
-    $("#text").html(data);
+    $("#text").html(data["text"]);
+    $("#cat").html(data["category"]);
     console.log($("#text").html(data)) 
-    alert("now?")
   });
 });
